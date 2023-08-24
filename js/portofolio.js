f3369281c46870d5f43fe9221002a2ac91ec5f2f
elouantheot <524115@student.fontys.nl>
2023-08-24 22:24:30 +0200

    Portofolio progressed + start of haption

---
+++ js/portofolio.js
@@ -0,0 +1,8 @@
+// script.js
+$(document).ready(function() {
+    // Load and insert the menu content
+    $("#horizontalmenu").load("generic-patterns/horizontalmenu.html");
+
+    // Load and insert the footer content
+    $("#footer").load("generic-patterns/footer.html");
+});
