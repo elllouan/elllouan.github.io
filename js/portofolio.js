e88326aa25a5363afcf47605cb2647d0b4bcb358
elouantheot <524115@student.fontys.nl>
2023-08-24 22:45:52 +0200

    made it more generic for the future

--- js/portofolio.js
+++
@@ -1,8 +0,0 @@
-// script.js
-$(document).ready(function() {
-    // Load and insert the menu content
-    $("#horizontalmenu").load("generic-patterns/horizontalmenu.html");
-
-    // Load and insert the footer content
-    $("#footer").load("generic-patterns/footer.html");
-});
