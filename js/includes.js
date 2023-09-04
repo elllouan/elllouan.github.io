07079e512f556782fe0a8d3345415ded7af38c13
elouantheot <524115@student.fontys.nl>
2023-09-04 22:39:51 +0200

    progressed on the internship page

--- js/includes.js
+++ js/includes.js
@@ -1,8 +1,8 @@
 // script.js
 $(document).ready(function() {
     // Load and insert the menu content
-    $("#horizontalmenu").load("generic-patterns/horizontalmenu.html");
+    $("#horizontalmenu").load("generic-patterns/html/horizontalmenu.html");
 
     // Load and insert the footer content
-    $("#footer").load("generic-patterns/footer.html");
+    $("#footer").load("generic-patterns/html/footer.html");
 });
