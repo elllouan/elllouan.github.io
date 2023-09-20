6d7f3c3b727fab0f39931d7d49af47d41e9ab6f0
elouantheot <524115@student.fontys.nl>
2023-09-20 14:32:57 +0200

    got further on the accordion (school page)

--- js/includes.js
+++ js/includes.js
@@ -6,3 +6,11 @@ $(document).ready(function() {
     // Load and insert the footer content
     $("#footer").load("generic-patterns/html/footer.html");
 });
+
+const accordion = document.getElementsByClassName('container');
+
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
+}
