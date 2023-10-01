63775a2f5c73ac3c8159553dae36c9d5f8b1aa47
elouantheot <524115@student.fontys.nl>
2023-10-01 15:04:06 +0200

    Improved a lot: main.css, school, index ...

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Define the countWhenClicked function
+  function count() {
+    counter++;
+    $('#counter').html(counter);
+    return count;
+  }
+
+  function reset() {
+    counter = 0;
+    $('#counter').html(counter);
+    return reset;
+  }
+
+  // Create a clickCounter by calling countWhenClicked()
+  var counter = 0;
+
+  // Attach a click event handler to the 'clickMe' button
+  $('#clickMe').click(function() {
+    count(); // This will increment and display the count
+  });
+
+  $('#resetMe').click(function() {
+    reset(); // This will reset the count to zero
+  });
+
+  function swapTextAfterDelay(textElement, newText, delay) {
+      setTimeout(function() {
+          textElement.text(newText);
+      }, delay);
+  }
+
+  const originalTextElement = $(".header-description");
+  const originalText = originalTextElement.text();
+  const newText = "Upcoming event !";
+  const delay = 3000;
+
+  let i = 0;
+
+  function swapText() {
+      if(i) swapTextAfterDelay(originalTextElement, originalText, delay);
+      else swapTextAfterDelay(originalTextElement, newText, delay);
+      i = (i + 1) % 2; // Assuming you want to toggle between original and new text
+  }
+
+  // Call swapText to start the swapping
+  setInterval(swapText, delay);
+
+});
+
+
+$(document).ready(function() {
+    // Load and insert the menu content
+    $("#horizontalmenu").load("generic-patterns/html/horizontalmenu.html");
+
+    // Load and insert the footer content
+    $("#footer").load("generic-patterns/html/footer.html");
+
+    $(".subject-container").on("click", function() {
+      $(this).toggleClass("active");
+    });
+
+    $(".current-project").on("click", function() {
+      $(this).toggleClass("active");
+    });
+
+});
